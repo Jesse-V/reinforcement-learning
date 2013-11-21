9e8e07b2b3027d4562aaa97b5d3d1f359aae515e
Jesse_V <jvictors@jessevictors.com>
2013-11-20 22:21:39 -0700

    initial project

---
+++ mouse-in-a-maze/src/RL/MouseInAMaze.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RL;
+
+/**
+ *
+ * @author jesse
+ */
+public class MouseInAMaze
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+}
